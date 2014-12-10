e2a544ed06867f5e20f3b7e7cff1dc6932e503a8
Matthew <m.hanhimaki@gmail.com>
2014-12-09 21:47:17 -0500

    Create injection.sh

---
+++ injection.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#
+#
+cp -r ~/android/prime_injection/app/. ~/android/aosp_lp/out/target/product/hammerhead/system/app/
+#
+cp -r ~/android/prime_injection/etc/. ~/android/aosp_lp/out/target/product/hammerhead/system/etc/
+#
+cp -r ~/android/prime_injection/framework/. ~/android/aosp_lp/out/target/product/hammerhead/system/framework/
+#
+cp -r ~/android/prime_injection/lib/. ~/android/aosp_lp/out/target/product/hammerhead/system/lib/
+#
+cp -r ~/android/prime_injection/priv-app/. ~/android/aosp_lp/out/target/product/hammerhead/system/priv-app/
+#
+cp -r ~/android/prime_injection/vendor/. ~/android/aosp_lp/out/target/product/hammerhead/system/vendor/
+
