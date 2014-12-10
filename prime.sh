6af5f245ec1803b9d188cef84020594d288f825c
Matthew <m.hanhimaki@gmail.com>
2014-12-10 08:35:13 -0500

    change path

--- prime.sh
+++ prime.sh
@@ -4,4 +4,4 @@
 #
 #
 #
-repo sync && cd kernel/msm && make clean && export ARCH=arm && export SUBARCH=arm && make hammerhead_defconfig && make -j10 && cd && cd android/aosp_lp && . build/envsetup.sh && lunch aosp_hammerhead-userdebug && make clean && make -j10 && ./injection.sh && make -j10 otapackage
+repo sync && cd kernel/msm && make clean && export ARCH=arm && export SUBARCH=arm && make hammerhead_defconfig && make -j10 && cd && cd android/lollipop && . build/envsetup.sh && lunch aosp_hammerhead-userdebug && make clean && make -j10 && ./injection.sh && make -j10 otapackage
